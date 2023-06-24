ab74b3427c9c6753872af023c252e2b656ad50fd
R Nithya Sree <nithyasreeramireddy@gmail.com>
2023-06-24 06:04:22 +0530

    learnt about variables

--- ProgrammingWithMosh/JS-Basics/index.js
+++ ProgrammingWithMosh/JS-Basics/index.js
@@ -1,2 +1,11 @@
 // Comment
 console.log('Hello World');
+let name = 'Nithya';
+console.log(name);
+
+// Variables cannot be reserved keywords
+// Should be meaningful
+// Cannot start with numbers
+// Cannot contain a space or hyphen
+// Are case sensitive
+
